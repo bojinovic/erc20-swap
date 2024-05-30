1c4a64a7d5cf6f760dcbbbaa8daa05f030a6d7f4
Milos Bojinovic <97438916+bojinovic@users.noreply.github.com>
2024-05-30 09:20:17 +0000

    cmt

--- test/ERC20Swapper.t.sol
+++ test/ERC20Swapper.t.sol
@@ -25,41 +25,31 @@ contract ERC20Swapper_Test is Test {
         tokenInfo.push(TokenInfo({token: ARB, price: 3000, decimals: 18}));
     }
 
-    function test_swaps(uint etherAmount, uint extraAmount) public {
-        etherAmount = 1;
-        uint weiAmount = uint(etherAmount) * 1 ether;
+    // -------------- Test suite
 
-        extraAmount = extraAmount % (10**15);
-        // weiAmount += extraAmount;
+    function test_swaps(uint weiAmount) public {
+        weiAmount = uint(weiAmount) % 10**19;
 
         for (uint i; i < tokenInfo.length; ++i) {
-
 
             uint minAmount = (weiAmount * tokenInfo[i].price * (10 ** tokenInfo[i].decimals)) / 10**18;
             console2.log("token:", tokenInfo[i].token, "minAmount", minAmount);
 
-            vm.startPrank(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+            vm.startPrank(user);
             uint received = erc20Swapper.swapEtherToToken{value: weiAmount}(tokenInfo[i].token, minAmount);
 
-
-            //assertGt(received, minAmount);
+            assertGt(received, minAmount);
 
             console2.log(received, minAmount);
         }
     }
 
+    // -------------- Implementation specific 
 
-    // function test_1() public {
-    //     uint etherValue = 10**18 + 1;
-    //     uint minAmount = 3850*10**18;
+    ERC20Swapper erc20Swapper;
 
-    //     vm.startPrank(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-    //     uint received = erc20Swapper.swapEtherToToken{value: etherValue}(DAI, minAmount);
-
-    //     console2.log(received, minAmount);
-    // }
-
-    ERC20Swapper internal erc20Swapper;
+    //randomly chosen account with enough ETH mainnet balance
+    address user = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     struct TokenInfo {
         address token;
@@ -72,5 +62,4 @@ contract ERC20Swapper_Test is Test {
     address constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address constant ARB = 0xB50721BCf8d664c30412Cfbc6cf7a15145234ad1;
-
 }
