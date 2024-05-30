d51efea33663b1df6fd2da5c8e59d14531735ae8
Milos Bojinovic <97438916+bojinovic@users.noreply.github.com>
2024-05-30 11:36:10 +0000

    cmt

--- dep-install.sh
+++
@@ -1,6 +0,0 @@
-forge install  https://github.com/foundry-rs/forge-std
-forge install  https://github.com/OpenZeppelin/openzeppelin-contracts
-forge install  https://github.com/Uniswap/uniswap-v3-core
-forge install  https://github.com/Uniswap/uniswap-v3-periphery
-forge install https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable
-forge install https://github.com/OpenZeppelin/openzeppelin-foundry-upgrades
